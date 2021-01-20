cd1652c4afa8a0d58c1e85296386767ea8021dad
Baeyeonggi <byg5913@gmail.com>
2021-01-20 15:49:51 +0900

    request API with graph-ql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,8 @@
-import { getMovies, getMovieId, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./db";
 
 export const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getMovieId(id)
-  },
-  Mutation: {
-    addMovie: (_, {name, score}) => addMovie(name, score),
-    deleteMovie: (_, {id}) => deleteMovie(id)
+    movies: (_, {rating, limit}) => getMovies(limit, rating)
   }
 };
 
